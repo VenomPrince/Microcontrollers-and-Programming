ed94b7b0d29edced4792034108f1033af422ea83
Prince <princekhand09@gmail.com>
2025-03-27 11:18:40 +0000

    Lab 1: String_Class_Oprations

---
+++ String_Class_Oprations.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    // Convert string to uppercase
+    for (int i = 0; i < str.length(); i++)
+    {
+        str[i] = toupper(str[i]);
+    }
+
+    // Count the number of vowels
+    int count = 0;
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+        {
+            count++;
+        }
+    }
+
+    // Reverse the string
+    string reversedStr = "";
+    for (int i = str.length() - 1; i >= 0; i--)
+    {
+        reversedStr += str[i];
+    }
+
+    // Print the results
+    cout << "Uppercase string: " << str << endl;
+    cout << "Number of vowels: " << count << endl;
+    cout << "Reversed string: " << reversedStr << endl;
+
+    return 0;
+}
