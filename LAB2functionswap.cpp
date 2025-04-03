defd0c158c7bcb2bc420a82e16594898d9348636
Prince <princekhand09@gmail.com>
2025-04-03 10:32:08 +0000

    Some fix on Functionswap

--- LAB2functionswap.cpp
+++ LAB2functionswap.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 using namespace std;
 
-void swap(int *a, int *b) {
-    int temp;
-    temp = *a;
-    *a = *b;
-    *b = temp;
+void multiply(double *b, double *a) {
+    cout << *b << endl;
+    cout << *a << endl;
+    cout << *b * *a << endl;
 }
 
 int main() {
-    int a = 5;
-    int b = 10;
-    swap(&a, &b);
+    double a = 5;
+    double b = 10;
+    multiply(&a, &b);
     cout << "a: " << a << ", b: " << b << endl;
     return 0;
 }
--- Lab:2_Overload.cpp
+++ Lab:2_Overload.cpp
@@ -1,26 +1,17 @@
 #include <iostream>
 using namespace std;
-void swapa(int *a)
+void multiply(double &b, double &e)
 {
-    cout<<*a<<endl;
-}
-void swapb(double *b, double *e)
-{
-    cout<<*b<<endl;
-    cout<<*e<<endl;
-}
-void swapc(int *c)
-{
-    cout<<*c<<endl;
+    cout<<b<<endl;
+    cout<<e<<endl;
+    cout<<b * e<<endl;
+
 }
     int main()
 {
-    int a = 1;
     double b = 2;
-    int c = 3;
     double e = 4;
-    swapa(&a);
-    swapb(&b, &e);
-    swapc(&c);
+    multiply(b, e);
+
     return 0;
 }
