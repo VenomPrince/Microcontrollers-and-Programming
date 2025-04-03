99e0dcb94b47d0846d7646d8bda461006913f8c7
Prince <princekhand09@gmail.com>
2025-04-03 10:35:10 +0000

    Some fix on Functionswap

--- LAB2functionswap.cpp
+++ LAB2functionswap.cpp
@@ -6,8 +6,16 @@ void multiply(double *b, double *a) {
     cout << *a << endl;
     cout << *b * *a << endl;
 }
+void multiply(int *c, int *d) {
+    cout << *c << endl;
+    cout << *d << endl;
+    cout << *c * *d << endl;
+}
+
 
 int main() {
+    int c = 5;
+    int d = 10;
     double a = 5;
     double b = 10;
     multiply(&a, &b);
